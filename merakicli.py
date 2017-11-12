48514fee591a5271e01f85be5f4eabf4aeab8533
mchenetz <mike>
2017-11-12 13:38:33 -0500

    Added some basic error checking and some fixes

--- merakicli.py
+++ merakicli.py
@@ -18,25 +18,34 @@ baseAPI = "https://" + merakiURL.get('base') + merakiURL.get('API')
 
 def merakiGet(path):
     mget = requests.get(baseAPI + path, headers=headers)
-    return json.dumps(mget.json())
+    #print (mget.url)
+    if int(mget.status_code) == 404:
+        return '{"status":"page does not exist"}'
+    else:
+        return json.dumps(mget.json())
 def merakiPut(merakiJson, path):
-    return requests.put(baseAPI + path, data=merakiJson, headers=headers)
+    mput = requests.put(baseAPI + path, data=merakiJson, headers=headers)
+    if mput.status_code != 200:
+        return '{"status":"error"}'
+    else:
+        return json.dumps(mput.json())
 def merakiPost(merakiJson, path):
     return requests.post(baseAPI + path, data=merakiJson, headers=headers)
 def merakiDel():
     pass
 
-
+#CLI parsing
 if sys.argv[1] == 'get':
     for arg in sys.argv[2:]:
-        totalarg = totalarg + arg + "/"
+        totalarg = totalarg + arg
+        # Parse last slash out
+        if arg != sys.argv[-1]:
+            totalarg = totalarg + "/"
     print(merakiGet(totalarg))
 if sys.argv[1] == 'put':
     for arg in sys.argv[3:]:
         totalarg = totalarg + arg + "/"
     result = merakiPut(json.loads(sys.argv[2]), totalarg)
-    print (totalarg)
-    print (sys.argv[2])
     print(result)
 if sys.argv[1] == 'post':
     for arg in sys.argv[3:]:
